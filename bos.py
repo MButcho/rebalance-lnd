7bbeff12220eff7d6bdc5d36dce9d9c0be86a19d
MButcho <mbutcho@pm.me>
2023-08-30 14:03:09 +0200

    Add bos rebalance

--- bos.py
+++ bos.py
@@ -4,13 +4,15 @@ import os
 import logging
 import subprocess
 import time
+import sys
 from datetime import datetime, timedelta
 
-bos_file_path = '/data/rebalance-lnd/bos.conf'
+script_path = os.path.dirname(sys.argv[0])
+bos_file_path = script_path+'/bos.conf'
 bos_file = open(bos_file_path, 'r')
 vampires_l = bos_file.read().split('\n')
 vampires = [x for x in vampires_l if x != '']
-logging.basicConfig(filename="/home/lnd/.bos/bos.log", format='%(asctime)s [%(levelname)s] %(message)s', datefmt='%Y/%m/%d %H:%M:%S', level=logging.INFO)
+logging.basicConfig(filename=script_path+"/bos.log", format='%(asctime)s [%(levelname)s] %(message)s', datefmt='%Y/%m/%d %H:%M:%S', level=logging.INFO)
 cron = 120 # running every 120 min
 #minutes = 15
 minutes = round(cron / len(vampires)) - 1
@@ -26,8 +28,9 @@ for str_line in vampires:
         ratio = round(float(str_line_arr[2]))
         events = round(int(str_line_arr[3]))
         
-        target_ratio = 10-(ratio/7.5) # check https://www.desmos.com/calculator
-        event_ratio = 2-(events/5) # check https://www.desmos.com/calculator
+        target_ratio = 7.5+(ratio/15) # check https://www.desmos.com/calculator
+        #target_ratio = 10-(ratio/7.5) # check https://www.desmos.com/calculator
+        event_ratio = 1.5-(events/20) # check https://www.desmos.com/calculator
         if event_ratio < 1:
             event_ratio = 1
         target_ratio = target_ratio/event_ratio
@@ -37,8 +40,8 @@ for str_line in vampires:
         amount = 2500 * 1000
         
         start_time = datetime.now()        
-        logging.info("Rebalancing " + alias + " started (amount: " + str(round(amount/1000)) + "k, target ppm: " + str(target_ppm) + "(-" + str(fee_delta) + "), events: " + str(events) + ")")
-        command = "/usr/bin/bos rebalance --in '" + alias + "' --out sources --max-fee-rate " + str(target_ppm) + " --max-fee 5000 --avoid-high-fee-routes --avoid vampires --minutes " + str(minutes) + " --amount " + str(amount) + " >> /home/lnd/.bos/bos_raw.log"
+        logging.info("Rebalancing " + alias + " started (a: " + str(round(amount/1000)) + "k, t: " + str(target_ppm) + "(-" + str(fee_delta) + "/-" + str(round(target_ratio,1)) + "%), e: " + str(events) + ")")
+        command = "/usr/bin/bos rebalance --in '" + alias + "' --out sources --max-fee-rate " + str(target_ppm) + " --max-fee 5000 --avoid-high-fee-routes --avoid vampires --minutes " + str(minutes) + " --amount " + str(amount) + " >> " + script_path + "/bos_raw.log"
         result = os.system(command)
         end_time = datetime.now()
         delta_min = round((end_time - start_time).total_seconds() / 60)
